b59cbb86de9a1e778bf6b9029b5511d525cdac37
Esteban Palacio <estipalacio@gmail.com>
2021-09-15 15:24:50 -0400

    added a filtered const for monsters based on search input value

--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ import './App.css';
 
 class App extends Component {
     constructor() {
-        super();
+        super()
 
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: '',
         }
     }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response =>response.json())
-            .then(users => this.setState({ monsters: users }));
+            .then(users => this.setState({ monsters: users }))
     }
   
     render() {
+        const { monsters, searchField } = this.state
+        const filteredMonsters = monsters.filter(monster =>
+            monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
         return (
         <div className="App">
-            < CardList monsters={this.state.monsters}/>
+            <input 
+                type="search" 
+                placeholder="search monsters" 
+                onChange={e =>  this.setState({ searchField: e.target.value })}
+            />
+            < CardList monsters={filteredMonsters}/>
         </div>
         )
     }   
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ import './card.styles.css'
 export const Card = props => (
     <div className='card-container'>
         <img 
-            src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
+            src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`} 
             alt="monster"
         />
         <h2>{props.monster.name}</h2>
