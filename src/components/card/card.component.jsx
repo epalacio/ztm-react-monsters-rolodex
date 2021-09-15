4a21df9c89cedb585be54d88a6f3f62903974af6
Esteban Palacio <estipalacio@gmail.com>
2021-09-15 10:56:44 -0400

    updated card components

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import './card.styles.css'
 
-export const Card = (props) => (
+export const Card = props => (
     <div className='card-container'>
         <img 
             src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
